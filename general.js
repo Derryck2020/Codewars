1aea8d2d495275edfab478b99f4ad79e5c815b8b
Xir Derryck <oseiagyemanb@gmail.com>
2022-09-27 21:06:33 +0100

    file update

--- general.js
+++ general.js
@@ -175,3 +175,37 @@ function canCharmanderBattle(currentValue){
         console.log('Charmander cannot battle')
     }
 }
+
+
+
+//Question 11
+// Your task is to split the chocolate bar of given dimension (n x m) into small squares. 
+// Each square is of size (1 x 1) and unbreakable. Implement a function that will return minimum number of breaks needed.
+// For example if you are given a chocolate bar of size (2 x 1) you can split it to single squares in just one break, 
+// but for size (3 x 1) you must do two breaks. If input data is invalid you should return 0 (as in no breaks are 
+// needed if we do not have any chocolate to split). Input will always be a non-negative integer.
+
+//Answer
+let breakChocolate = function(n, m) {
+    if(n === 0 || m === 0) {
+        return 0
+    } else{
+        return n*m - 1;
+    }
+};
+console.log(breakChocolate(5,5)) // 24
+
+//Alternative Answer
+let crackChocolate = (n, m) => (n*m === 0) ? 0 : n * m - 1;
+console.log(crackChocolate(5,5))
+/*
+(Here we use a ternary operator which is used in place of if...else statement)
+Syntax: condition ? exprIfTrue : exprIfFalse
+Note: Besides false, possible falsy expressions are: null, NaN, 0, the empty string (""), and undefined
+*/
+
+
+
+//Question 12
+
+
